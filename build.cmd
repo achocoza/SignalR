86ef668296536c365a23f69c05042ec672e8ec8b
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-08-13 16:23:18 -0700

    more spelunking

--- build.cmd
+++ build.cmd
@@ -1,2 +1,6 @@
 @ECHO OFF
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0run.ps1' default-build %*; exit $LASTEXITCODE"
+
+cd \
+dir /s sn.exe
+
+REM PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0run.ps1' default-build %*; exit $LASTEXITCODE"
