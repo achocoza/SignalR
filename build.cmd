b994bcd6a19e68d5ed4ef0f98b0b28b78b9de09e
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-08-13 14:52:41 -0700

    go spelunking

--- build.cmd
+++ build.cmd
@@ -1,2 +1,9 @@
 @ECHO OFF
+
+REM Hacky testing
+dir "C:\Program Files (x86)"
+dir "C:\Program Files (x86)\Microsoft Visual Studio"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2017"
+
+
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0run.ps1' default-build %*; exit $LASTEXITCODE"
