e5bca3b5397e7eaf59303f66e4ddbb0266d25816
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-08-14 09:22:13 -0700

    found it!

--- build.cmd
+++ build.cmd
@@ -1,7 +1,2 @@
 @ECHO OFF
-
-C:
-cd \
-dir /s sn.exe
-
-REM PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0run.ps1' default-build %*; exit $LASTEXITCODE"
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0run.ps1' default-build %*; exit $LASTEXITCODE"
